12dcb9edaeacf240fe5d09eef57285d04821a717
Stefanus Wirdatmadja <stefanus.wirdatmadja@student.tut.fi>
2018-09-05 18:43:52 +0300

    factorial_01

---
+++ student/01/factorial/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+const unsigned int MAX = 33;
+
+int main()
+{
+    unsigned int factor = 0;
+    unsigned long long result = 1;
+    std::cout << "Enter a positive integer (from interval [1.." << MAX << "]): ";
+    std::cin >> factor;
+    if (factor > MAX){
+        std::cout << "Too big number" << std::endl;
+    }
+    else if (factor <= 1){
+        std::cout << "Factorial of " << factor << " is " << 0 << std::endl;
+    }
+    else{
+        for (int a=1; a <= factor; ++a){
+            result *= a;
+        }
+        std::cout << "Factorial of " << factor << " is " << result << std::endl;
+    }
+}
