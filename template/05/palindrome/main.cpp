963ba3e412d54e1914f16515f31e7123785ad3b2
Maarit Harsu <maarit.harsu@tut.fi>
2018-09-24 10:02:32 +0300

    Round 5 published

---
+++ template/05/palindrome/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+
+
+int main()
+{
+    std::cout << "Enter a word: ";
+    std::string word;
+    std::cin >> word;
+
+    if(palindrome_recursive(word)){
+        std::cout << word << " is a palindrome" << std::endl;
+    } else {
+        std::cout << word << " is not a palindrome" << std::endl;
+    }
+}
